a70bf0bddd3ff2b8eb21e04b5f5142e95613a184
Tomdatbenik <tomvankaathoven@gmail.com>
2024-03-20 20:01:26 +0100

    return model its called from to not change normal fields

---
+++ __tests__/mapFrom.test.ts
@@ -0,0 +1,25 @@
+import { MapFrom } from "../src/decorators/mapFrom";
+import { Exclude, Model } from "../src/index";
+
+describe("mapFrom", () => {
+  class MapFromModel extends Model {
+    @MapFrom("source_field")
+    public test: undefined | string;
+
+    public leftAlone = "left alone";
+  }
+
+  describe("fromJson", () => {
+    it("should map from json", () => {
+      const model = new MapFromModel();
+
+      const json = JSON.stringify({
+        source_field: "value",
+      });
+
+      const actual = model.fromJson<MapFromModel>(json);
+
+      expect(actual.leftAlone).toBe("left alone");
+    });
+  });
+});
