cc3d2926bdbf130b00336d60288f21ba03fa1759
Tomdatbenik <tomvankaathoven@gmail.com>
2024-03-26 13:07:37 +0100

    remove context check

--- src/decorators/restClient.decorator.ts
+++ src/decorators/restClient.decorator.ts
@@ -3,8 +3,6 @@ import { ApiClient } from "./../classes/client.base";
 
 export function RestClient(baseUrl: string) {
   return function (target: typeof ApiClient, context: ClassDecoratorContext) {
-    if (context.kind === "class") {
-      return setClientMeta(target, "_baseUrl", baseUrl);
-    }
+    return setClientMeta(target, "_baseUrl", baseUrl);
   };
 }
